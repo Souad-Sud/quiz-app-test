8f0f72991424e4cfdde085d40bce049dd58ed4ec
todatoday <taki.souad-90@hotmail.com>
2025-08-22 12:15:52 +0200

    merge

--- src/component/OptionList/index.tsx
+++ src/component/OptionList/index.tsx
@@ -1,10 +1,37 @@
-const OptionList = () => {
-    return(
-        <div>
-            Option list
+'use client'
+import React from "react";
+import OptionButton from "../OptionButton";
+import questions from "@/data/data";
 
-        </div>
-    )
-}
+type OptionListProps = {
+  handleClick: (option: string) => void;
+  userAnswer: string | null;
+  rightAnswer: string;
+  questionNumber: number;
+};
 
-export default OptionList
+const OptionList: React.FC<OptionListProps> = ({
+  handleClick,
+  userAnswer,
+  rightAnswer,
+  questionNumber,
+}) => {
+  const options = questions[questionNumber].options;
+
+  return (
+    <div>
+      {options.map((option) => (
+        <OptionButton
+          key={option}
+          option={option}
+          handleClick={handleClick}
+          userAnswer={userAnswer}
+          rightAnswer={rightAnswer}
+          disabled={!!userAnswer} 
+        />
+      ))}
+    </div>
+  );
+};
+
+export default OptionList;
